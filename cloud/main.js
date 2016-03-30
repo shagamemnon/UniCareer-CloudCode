c3fe333adc23cf7159838fd43adb9330ad743631
daisy <daisyramos317@gmail.com>
2016-03-30 12:30:41 -0400

    adding beforeSave trigger on Experience

--- cloud/main.js
+++ cloud/main.js
@@ -4,3 +4,28 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+
+Parse.Cloud.beforeSave("Experience", function(request, response){
+
+var newEntry = request.object;
+
+var checkPrevious = new Parse.Query("Experience");
+checkPrevious.equalTo("positionTitle", newEntry.get("positionTitle"));
+checkPrevious.equalTo("companyName", newEntry.get("companyName"));
+
+checkPrevious.first({
+	success: function(object) {
+	
+	if(object){
+		 response.error({errorCode: 123, errorMsg: "Already entered this value"});
+	} else {
+		response.success();
+	}
+	},
+	error: function(error) {
+		response.error("Could not validate uniqueness of object");
+	}
+});
+});
+
