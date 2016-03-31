9666b7257b910f884f76caa1482f0a293f9f89b5
daisy <daisyramos317@gmail.com>
2016-03-31 12:26:51 -0400

    more Stripe functionality

--- cloud/main.js
+++ cloud/main.js
@@ -1,7 +1,5 @@
 //Stripe Test SecretKey
-
 var Stripe = require('stripe')
-
 Stripe.initialize('sk_test_ePDSBK4YqUagVl3dkjzjpDt1');
 
 var stripeSecretKey = 'sk_test_ePDSBK4YqUagVl3dkjzjpDt1';
@@ -16,7 +14,7 @@ Parse.Cloud.define("saveStripeCustomerIdAndCharge", function (request, response)
                 success: function(customer) {
  
                             var User = request.user;
-                            User.set("stripe_customer_id",customer.id );
+                            User.set("stripeCustomerId",customer.id );
                             User.save(null, {
                                 success: function(customer){
  
@@ -48,7 +46,85 @@ Parse.Cloud.define("saveStripeCustomerIdAndCharge", function (request, response)
 });
 });
 
+Parse.Cloud.define("stripeDeleteCard", function(request, response)
+{   
+    Parse.Cloud.httpRequest({
+       method:"DELETE",
+       url: "https://" + stripeSecretKey + ':@' + stripeBaseUrl + "/customers/" + request.params.customerId + "/sources/" + request.params.cardId,
+      // body: "id="+request.params.cardId,
+        success: function(cards) {
+        console.log(JSON.stringify(cards));
+            response.success(cards);
+        },
+        error: function(httpResponse) {
+            response.error('Request failed with response code ' + httpResponse.status);
+        }
+    });
+});
 
+Parse.Cloud.define("stripeGetDefaultCard", function (request, response){
+ 
+Stripe.Customers.retrieve(request.params.customerId,{ 
+                        expand: ["data.source"], 
+                        success: function(stripeCustomer){
+ 
+                            console.log("Customer info received: "+ JSON.stringify(stripeCustomer));
+ 
+ 	            var creditCardResponse = {
+ 
+                                    "id"                : stripeCustomer.id,
+                                    "cardId"            : stripeCustomer.sources.data[0].id,
+                                    "cardHolderEmail"   : stripeCustomer.email,
+                                    "cardBrand"         : stripeCustomer.sources.data[0].brand,
+                                    "cardLast4Digits"   : stripeCustomer.sources.data[0].last4
+ 
+                                };
+                           response.success(creditCardResponse);
+          
+                        }, error: function(error){
+ 
+                            console.log("Failed to Fetch Stripe Customer for Stripe customerId: "+customerId);
+                            console.log("error "+JSON.stringify(error));
+
+                            var error = new Parse.Error(Parse.Error.OTHER_CAUSE, "Fetch Stripe Customer Failed");
+                            response.error(error);
+ 
+                        }
+                    });
+ 
+});
+
+Parse.Cloud.define("stripeAddCardToCustomer", function(request, response) 
+{ 
+        Parse.Cloud.httpRequest({
+                method:"POST",
+                url: "https://" + stripeSecretKey + ':@' + stripeBaseUrl + "/customers/" + request.params.customerId + "/cards",
+                body: "card="+request.params.tokenId,
+         
+        //check for duplicate fingerPrintIds
+                success: function(httpResponse) {
+        var jsonObj = JSON.parse(httpResponse.text)
+        var jsonResult = (jsonObj['fingerprint']);
+        console.log("New fingerprint:" + jsonObj['fingerprint']);
+        console.log(httpResponse.text);     
+        var cardResponse = {
+         
+        "fingerPrint":  jsonResult,
+        };
+         
+               response.success(cardResponse);
+        //response.success(httpResponse.text);
+         
+                },
+                error: function(httpResponse) {
+                response.error('Request failed with response code ' + httpResponse.status);
+                }
+        });
+ 
+     
+});
+
+/*
 Parse.Cloud.beforeSave("Experience", function(request, response){
 
 var newEntry = request.object;
@@ -71,4 +147,4 @@ checkPrevious.first({
 	}
 });
 });
-
+*/
